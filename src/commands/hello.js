233a3f0dfb48b5d6cbe2377db1c5f239a2d06bd4
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2024-07-24 17:20:59 +0000

    rf(hello): added guild keyword -> It's optionnal use so not using it will default to global status

--- src/commands/hello.js
+++ src/commands/hello.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js')
 
 module.exports = {
+    /* Set to true if it should only be available to guilds specified in the .env */
+    guildOnly: false,
     register_command: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Displays all commands available to you !'),
