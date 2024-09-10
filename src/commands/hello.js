d8e3eed573207199106bb972852e7c49a8bcf0ce
Maximus7474 <the.maximus.7474@gmail.com>
2024-09-10 16:40:25 +0200

    fix(hello): command is called help but should be hello

--- src/commands/hello.js
+++ src/commands/hello.js
@@ -1,11 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const Discord = require('discord.js')
 
 module.exports = {
     /* Set to true if it should only be available to guilds specified in the .env */
     guildOnly: false,
     register_command: new SlashCommandBuilder()
-        .setName('help')
+        .setName('hello')
         .setDescription('Displays all commands available to you !'),
     async execute(client, interaction) {
         return interaction.reply({content:`World!`,ephemeral :true})
