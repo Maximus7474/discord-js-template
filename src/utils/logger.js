5905a49173291d78b541676bda7177b85621a4a1
Maximus7474 <the.maximus.7474@gmail.com>
2024-09-10 17:22:24 +0200

    rf(logger): allow multiple argumnts

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,20 +4,21 @@ class Logger {
     constructor(origin) {
         this.origin = origin
     }
-    info(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[${this.origin}] [INFO]`)} ${message}`);
+
+    info(...message) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${(colors.cyan(`[INFO]`)+`    [${colors.blue(this.origin)}]`).padEnd(50, ' ')}`, ...message);
     }
 
-    success(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[${this.origin}] [SUCCESS]`)} ${message}`);
+    success(...message) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${(colors.green(`[SUCCESS]`)+` [${colors.blue(this.origin)}]`).padEnd(50, ' ')}`, ...message);
     }
 
-    warn(message) {
-        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[${this.origin}] [WARN]`)} ${message}`);
+    warn(...message) {
+        console.warn(`${colors.gray((new Date()).toLocaleString())} ${(colors.yellow(`[WARN]`)+`    [${colors.blue(this.origin)}]`).padEnd(50, ' ')}`, ...message);
     }
 
-    error(message) {
-        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[${this.origin}] [ERROR]`)} ${message}`);
+    error(...message) {
+        console.error(`${colors.gray((new Date()).toLocaleString())} ${(colors.red(`[ERROR]`)+`   [${colors.blue(this.origin)}]`).padEnd(50, ' ')}`, ...message);
     }
 }
 
