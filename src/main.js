bf49d25c67f9e734c6428dc39fbec3dace2c1029
Maximus7474 <the.maximus.7474@gmail.com>
2024-09-10 16:47:58 +0200

    chore(main): added initialize DB function

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const assert = require('assert');
 const find_events = require('./utils/initialisation/find_events');
 const setup_commands = require('./utils/initialisation/setup_commands');
+const { initializeDatabase } = require('./utils/database/sqliteHandler');
 
 assert(process.env.TOKEN, "A Discord Token for your bot is required ! Please go to your application page to get it! Set your token then as an enviormental variable with the TOKEN variable name!")
 
@@ -19,6 +20,8 @@ const client = new Discord.Client({
         Discord.Partials.Reaction
     ]
 });
+
+initializeDatabase();
 
 find_events(client);
 
