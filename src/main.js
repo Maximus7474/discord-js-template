59ff266b4b17fec2adf6b1a864da0b9a24e515ed
Maximus7474 <the.maximus.7474@gmail.com>
2024-09-10 16:50:23 +0200

    chore(main): added missing semi colons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js')
-require('dotenv').config()
-const assert = require('assert')
-const find_events = require('./utils/initialisation/find_events')
+const Discord = require('discord.js');
+require('dotenv').config();
+const assert = require('assert');
+const find_events = require('./utils/initialisation/find_events');
 const setup_commands = require('./utils/initialisation/setup_commands');
 
 assert(process.env.TOKEN, "A Discord Token for your bot is required ! Please go to your application page to get it! Set your token then as an enviormental variable with the TOKEN variable name!")
@@ -20,11 +20,11 @@ const client = new Discord.Client({
     ]
 });
 
-find_events(client)
+find_events(client);
 
 setup_commands(client);
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
 
-client.once(Discord.Events.ClientReady,(client)=>{
+client.once(Discord.Events.ClientReady, (client)=>{
 });
