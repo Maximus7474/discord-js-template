e36b598b7933f1305a983a12f23393a5fcf76160
Maximus7474 <the.maximus.7474@gmail.com>
2024-10-15 17:26:51 +0200

    rf(staticMessage): example file

--- src/utils/staticMessages/example.js
+++ src/utils/staticMessages/example.js
@@ -4,7 +4,14 @@ const log = new require('../logger.js');
 const logger = new log("Static Message");
 
 module.exports = {
-    customId: 'customId',
-    async setup (client) {},
-    async callback (client, interaction) {}
+    /* Can also by a simple string */
+    customId: ['customId'],
+    /* Setup function that is run on client start */
+    async setup (client) {
+        logger.info('Static Message has been setup')
+    },
+    /* The function called for any interactions created using the specified customId */
+    async callback (client, interaction) {
+        logger.warn('There is no callback function for \'customId\'');        
+    }
 }
