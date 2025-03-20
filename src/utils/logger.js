9592d5e0e9a687b34d4a39677ad8c46fde9e69dd
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-20 21:14:47 +0100

    feat(utils/logger): added jsdoc instructions

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,23 +1,47 @@
 const colors = require('colors/safe');
 
+/**
+ * Logger utility class to output formatted console logs with contextual origin and log levels.
+ */
 class Logger {
+    /**
+     * Create a new Logger instance.
+     * @param {string} origin - Identifier to indicate the source of the logs (e.g., module name).
+     */
     constructor(origin) {
-        this.origin = origin
-    }
-    info(...message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[${this.origin}] [INFO]`)}`, ...message);
+        this.origin = origin;
     }
 
-    success(...message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[${this.origin}] [SUCCESS]`)}`, ...message);
+    /**
+     * Log an informational message to the console.
+     * @param {...any} args - The arg(s) to log.
+     */
+    info(...args) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[${this.origin}] [INFO]`)}`, ...args);
     }
 
-    warn(...message) {
-        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[${this.origin}] [WARN]`)}`, ...message);
+    /**
+     * Log a success message to the console.
+     * @param {...any} args - The arg(s) to log.
+     */
+    success(...args) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[${this.origin}] [SUCCESS]`)}`, ...args);
     }
 
-    error(...message) {
-        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[${this.origin}] [ERROR]`)}`, ...message);
+    /**
+     * Log a warning message to the console.
+     * @param {...any} args - The arg(s) to log.
+     */
+    warn(...args) {
+        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[${this.origin}] [WARN]`)}`, ...args);
+    }
+
+    /**
+     * Log an error message to the console.
+     * @param {...any} args - The arg(s) to log.
+     */
+    error(...args) {
+        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[${this.origin}] [ERROR]`)}`, ...args);
     }
 }
 
