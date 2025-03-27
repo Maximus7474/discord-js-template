6124c5d06c1fb9ebbd4bf35c2beaf59f6a103cc6
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-27 14:39:22 +0100

    tweak(main): use new Client interface

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import assert from 'assert';
 import find_events from './utils/initialisation/find_events';
 import setup_commands from './utils/initialisation/setup_commands';
 import { InitializeStaticMessages } from './utils/initialisation/setup_staticMessages';
+import { Client } from './types/client';
 
 dotenv.config();
 
 assert(process.env.TOKEN, "A Discord Token for your bot is required ! Please go to your application page to get it! Set your token then as an enviormental variable with the TOKEN variable name!")
 
-const client = new Discord.Client({
+const client: Client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.Guilds,
         Discord.GatewayIntentBits.GuildMessages,
@@ -19,9 +20,11 @@ const client = new Discord.Client({
     partials: [
         Discord.Partials.Message,
         Discord.Partials.Channel,
-        Discord.Partials.Reaction
-    ]
+        Discord.Partials.Reaction,
+    ],
 });
+
+client.commands = new Discord.Collection();
 
 find_events(client);
 
